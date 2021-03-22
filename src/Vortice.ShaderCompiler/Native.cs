9e5b6691a528a005ac414d5e710a9f239bccca61
Amer Koleci <amerkoleci@gmail.com>
2021-03-22 12:18:28 +0100

    ENH: Vortice.ShaderCompiler targets net5 now as well and use NativeLibrary instead of own library loading.

--- src/Vortice.ShaderCompiler/Native.cs
+++ src/Vortice.ShaderCompiler/Native.cs
@@ -2,14 +2,12 @@
 // Distributed under the MIT license. See the LICENSE file in the project root for more information.
 
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace Vortice.ShaderCompiler
 {
     internal static unsafe class Native
     {
-        private static readonly ILibraryLoader s_loader = InitializeLoader();
         private static readonly IntPtr s_NativeLibrary = LoadNativeLibrary();
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
@@ -257,65 +255,25 @@ namespace Vortice.ShaderCompiler
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return LoadLibrary("shaderc_shared.dll");
+                return NativeLibrary.Load("shaderc_shared.dll");
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                return LoadLibrary("libshaderc_shared.so");
+                return NativeLibrary.Load("libshaderc_shared.so");
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                return LoadLibrary("libshaderc_shared.dylib");
+                return NativeLibrary.Load("libshaderc_shared.dylib");
             }
             else
             {
-                return LoadLibrary("shaderc_shared");
+                return NativeLibrary.Load("shaderc_shared");
             }
-        }
-
-        private static IntPtr LoadLibrary(string libname)
-        {
-            string? assemblyLocation = Path.GetDirectoryName(typeof(Native).Assembly.Location) ?? "./";
-            IntPtr ret;
-
-            // Try .NET Framework / mono locations
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, libname));
-
-                // Look in Frameworks for .app bundles
-                if (ret == IntPtr.Zero)
-                    ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, "..", "Frameworks", libname));
-            }
-            else
-            {
-                if (Environment.Is64BitProcess)
-                    ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, "x64", libname));
-                else
-                    ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, "x86", libname));
-            }
-
-            // Try .NET Core development locations
-            if (ret == IntPtr.Zero)
-                ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, "native", Rid, libname));
-
-            if (ret == IntPtr.Zero)
-                ret = s_loader.LoadLibrary(Path.Combine(assemblyLocation, "runtimes", Rid, "native", libname));
-
-            // Try current folder (.NET Core will copy it there after publish) or system library
-            if (ret == IntPtr.Zero)
-                ret = s_loader.LoadLibrary(libname);
-
-            // Welp, all failed, PANIC!!!
-            if (ret == IntPtr.Zero)
-                throw new Exception("Failed to load library: " + libname);
-
-            return ret;
         }
 
         private static T LoadFunction<T>(string function)
         {
-            IntPtr handle = s_loader.GetSymbol(s_NativeLibrary, function);
+            IntPtr handle = NativeLibrary.GetExport(s_NativeLibrary, function);
 
             if (handle == IntPtr.Zero)
             {
@@ -324,97 +282,6 @@ namespace Vortice.ShaderCompiler
 
             return Marshal.GetDelegateForFunctionPointer<T>(handle);
         }
-
-        private static ILibraryLoader InitializeLoader()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return new WindowsLoader();
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return new OSXLoader();
-            }
-            else
-            {
-                return new UnixLoader();
-            }
-        }
-
-        private static string Rid
-        {
-            get
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Environment.Is64BitProcess)
-                    return "win-x64";
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !Environment.Is64BitProcess)
-                    return "win-x86";
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                    return "linux-x64";
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                    return "osx";
-                else
-                    return "unknown";
-            }
-        }
-
-
-        internal interface ILibraryLoader
-        {
-            IntPtr LoadLibrary(string name);
-
-            IntPtr GetSymbol(IntPtr module, string name);
-        }
-
-        private class WindowsLoader : ILibraryLoader
-        {
-            public IntPtr LoadLibrary(string name) => LoadLibraryW(name);
-
-            public IntPtr GetSymbol(IntPtr module, string name) => GetProcAddress(module, name);
-
-            [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-            private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-            [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
-            private static extern IntPtr LoadLibraryW(string lpszLib);
-
-            [DllImport("kernel32")]
-            private static extern int FreeLibrary(IntPtr module);
-        }
-
-        private class UnixLoader : ILibraryLoader
-        {
-            private const int RTLD_LOCAL = 0x0000;
-            private const int RTLD_NOW = 0x0002;
-
-            public IntPtr LoadLibrary(string name) => dlopen(name, RTLD_NOW | RTLD_LOCAL);
-
-            public IntPtr GetSymbol(IntPtr module, string name) => dlsym(module, name);
-
-
-            [DllImport("libdl.so.2")]
-            private static extern IntPtr dlopen(string path, int flags);
-
-            [DllImport("libdl.so.2")]
-            private static extern IntPtr dlsym(IntPtr handle, string symbol);
-        }
-
-        private class OSXLoader : ILibraryLoader
-        {
-            private const int RTLD_LOCAL = 0x0000;
-            private const int RTLD_NOW = 0x0002;
-
-            public IntPtr LoadLibrary(string name) => dlopen(name, RTLD_NOW | RTLD_LOCAL);
-
-            public IntPtr GetSymbol(IntPtr module, string name) => dlsym(module, name);
-
-
-            [DllImport("/usr/lib/libSystem.dylib")]
-            private static extern IntPtr dlopen(string path, int flags);
-
-            [DllImport("/usr/lib/libSystem.dylib")]
-            private static extern IntPtr dlsym(IntPtr handle, string symbol);
-        }
-        #endregion  NativeLibrary Logic
+        #endregion
     }
 }
